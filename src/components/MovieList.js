690205bcda5b031d5de491dc5f7334f47e8d85ca
reinaldoriant <reinaldoriant@gmail.com>
2021-02-18 17:21:19 +0700

    collect data from API

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,7 +4,7 @@ const MovieList = (props) => {
 	return (
 		<>
 			{props.movies.map((movie, index) => (
-				<div className='image-container d-flex justify-content-start m-3'>
+				<div key={movie.imdbID} className='image-container d-flex justify-content-start m-3'>
 					<img src={movie.Poster} alt='movie'></img>
 				</div>
 			))}
