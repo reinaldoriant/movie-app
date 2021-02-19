8c019da37dd7df57914c6d88c5a58a6fde89e280
reinaldoriant <reinaldoriant@gmail.com>
2021-02-19 16:18:20 +0700

    add hover in image of movie

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
 
   return (<div className='container-fluid movie-app'>
     <div className='row d-flex align-items-center mt-4 mb-4'>
-      <MovieListHeading heading='Movies' />
+      <MovieListHeading heading='Movies App' />
       <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
     </div>
     <div className='row'>
